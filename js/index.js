78bd06e4d03963095ed1ae976dbff642e49e3c86
Moringa School <karanjasam210@gmail.com>
2022-03-14 15:45:34 +0300

    ci: add jQuery functions to toggle back to image when what we do description is clicked

--- js/index.js
+++ js/index.js
@@ -13,6 +13,23 @@
         $(".description3").toggle();
     })
 
+    $(".description1").click(function(){
+        $(".description1").toggle()
+        $(".clickable1").toggle();
+    })
+
+    $(".description2").click(function(){
+        $(".description2").toggle()
+        $(".clickable2").toggle();
+    })
+
+    $(".description3").click(function(){
+        $(".description3").toggle()
+        $(".clickable3").toggle();
+    })
+
+    
+
 
 
     
