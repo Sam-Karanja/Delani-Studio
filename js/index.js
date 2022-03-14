c46815e223a2941270d2567e1a01823d4b0352a0
Moringa School <karanjasam210@gmail.com>
2022-03-14 15:39:31 +0300

    ci: add jQuery functions to display what we do descriptions when image is clicked

---
+++ js/index.js
@@ -0,0 +1,24 @@
+    $(".clickable1").click(function(){
+        $(".clickable1").toggle()
+        $(".description1").toggle();
+    })
+
+    $(".clickable2").click(function(){
+        $(".clickable2").toggle()
+        $(".description2").toggle();
+    })
+
+    $(".clickable3").click(function(){
+        $(".clickable3").toggle()
+        $(".description3").toggle();
+    })
+
+
+
+    
+// showing a hidden image
+// $(document).ready(function(){
+//     $("p").click(function(){
+//         $("img").show();
+//     })
+// })
