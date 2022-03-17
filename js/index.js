d124e5840be7855be3a9deb25e945460a2ac3342
Moringa School <karanjasam210@gmail.com>
2022-03-17 09:55:28 +0300

    refactor: add indentation

--- js/index.js
+++ js/index.js
@@ -1,77 +1,68 @@
-    $(".clickable1").click(function(){
-        $(".clickable1").toggle()
-        $(".description1").toggle();
-    })
+$(".clickable1").click(function () {
+  $(".clickable1").toggle();
+  $(".description1").toggle();
+});
 
-    $(".clickable2").click(function(){
-        $(".clickable2").toggle()
-        $(".description2").toggle();
-    })
+$(".clickable2").click(function () {
+  $(".clickable2").toggle();
+  $(".description2").toggle();
+});
 
-    $(".clickable3").click(function(){
-        $(".clickable3").toggle()
-        $(".description3").toggle();
-    })
+$(".clickable3").click(function () {
+  $(".clickable3").toggle();
+  $(".description3").toggle();
+});
 
-    $(".description1").click(function(){
-        $(".description1").toggle()
-        $(".clickable1").toggle();
-    })
+$(".description1").click(function () {
+  $(".description1").toggle();
+  $(".clickable1").toggle();
+});
 
-    $(".description2").click(function(){
-        $(".description2").toggle()
-        $(".clickable2").toggle();
-    })
+$(".description2").click(function () {
+  $(".description2").toggle();
+  $(".clickable2").toggle();
+});
 
-    $(".description3").click(function(){
-        $(".description3").toggle()
-        $(".clickable3").toggle();
-    })
+$(".description3").click(function () {
+  $(".description3").toggle();
+  $(".clickable3").toggle();
+});
 
-    $(".project1").hover(function(){       
-        $(".project1-1").toggle()
-        $(".project1").toggle()
-    })
-    $(".project2").hover(function(){
-        $(".project1-2").toggle()
-        $(".project2").toggle()
-        })
-    $(".project3").hover(function(){
-        $(".project1-3").toggle()
-        $(".project3").toggle()  
-      })
-    $(".project4").hover(function(){
-        $(".project1-4").toggle()
-        $(".project4").toggle()  
-      })
-    $(".project5").hover(function(){
-        $(".project1-5").toggle()
-        $(".project5").toggle()  
-      })
-    $(".project6").hover(function(){
-        $(".project1-6").toggle()
-        $(".project6").toggle()  
-      })
-    $(".project7").hover(function(){
-        $(".project1-7").toggle()
-        $(".project7").toggle()  
-      })
-    $(".project8").hover(function(){
-        $(".project1-8").toggle()
-        $(".project8").toggle()  
-      })
+$(".project1").hover(function () {
+  $(".project1-1").toggle();
+  $(".project1").toggle();
+});
+$(".project2").hover(function () {
+  $(".project1-2").toggle();
+  $(".project2").toggle();
+});
+$(".project3").hover(function () {
+  $(".project1-3").toggle();
+  $(".project3").toggle();
+});
+$(".project4").hover(function () {
+  $(".project1-4").toggle();
+  $(".project4").toggle();
+});
+$(".project5").hover(function () {
+  $(".project1-5").toggle();
+  $(".project5").toggle();
+});
+$(".project6").hover(function () {
+  $(".project1-6").toggle();
+  $(".project6").toggle();
+});
+$(".project7").hover(function () {
+  $(".project1-7").toggle();
+  $(".project7").toggle();
+});
+$(".project8").hover(function () {
+  $(".project1-8").toggle();
+  $(".project8").toggle();
+});
 
-      
-    $("#submit").click(function(){
-        alert('Thank You for contacting us. We wil get back to you as soon as possible')     
-
-    })
-      
-
-
-
-    
-
-
-
-    
+$("#submit").click(function () {
+  alert(
+    "Thank You for contacting us. We wil get back to you as soon as possible"
+  );
+});
